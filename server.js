6b75c8771905daecdf97dc0093e6f838bdb381d9
jeregill <43596101+jeregill@users.noreply.github.com>
2020-07-09 11:46:13 -0700

    extending request limit

--- server.js
+++ server.js
@@ -44,11 +44,9 @@ const allowCrossDomain = function(req, res, next) {
   next();
 };
 app.use(allowCrossDomain);
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({limit: '50mb'} ));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true,
-}));
-app.use(bodyParser.json());
 
 /* Init Fake Data */
 
