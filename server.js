5a0a945f8b35990f35f3e71c45c7eeae3fd62886
jeregill <43596101+jeregill@users.noreply.github.com>
2020-07-27 17:17:36 -0700

    fixed cors bug, adjusted barber fake data

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ const allowCrossDomain = function (req, res, next) {
     }
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", "https://dibs-vancouver.herokuapp.com");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     next();
 };
