dac16428427814d8f32423a465a76c3cebdeffb5
jeregill <43596101+jeregill@users.noreply.github.com>
2020-08-03 16:06:11 -0700

    Removed error for zero reservations

--- query/customer.js
+++ query/customer.js
@@ -16,7 +16,6 @@ async function getStore(request, response) {
             throw "/query/customer/getStore: No stores found with given store_id";
         }
         if(noPictures){
-            console.log('here');
             store_result.pictures = null;
         }
         ret.store_id = store_result.store_id;
@@ -471,9 +470,6 @@ async function getReservations(request, response) {
 
     try {
         const reservation_results = await schema.Reservation.find(reservation_body).exec();
-        if (reservation_results.length === 0) {
-            throw "/query/customer/getReservations: No stores found with given store_id";
-        }
         ret.reservations = reservation_results;
 
         return response.status(200).send(ret);
